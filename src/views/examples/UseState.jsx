d3f8c2dc36c886230a72958732a506d644cdb538
Andreza Nóbrega <ancarvalho@ecomp.uefs.br>
2023-01-09 19:54:17 -0300

    UseState exercicio 1

---
+++ src/views/examples/UseState.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
+
+const UseState = (props) => {
+/*     0 é o valor inicial
+    count é o valor atual
+    setCount é a função que altera o estado 
+    como a seguir:
+    const [state, setState] = useState(initialState)
+    */
+    const [count, setCount] = useState(0)
+
+    return (
+
+        <div className="UseState">
+            <PageTitle
+                title="Hook UseState"
+                subtitle="Estado em componentes funcionais!"
+            />
+            <SectionTitle title="Exercício #01"/>
+            <div className="center">
+                <span className="text">{count}</span>
+                <div>
+                    <button className="btn"
+                    onClick={()=> setCount(count - 1)}>-1</button>
+                    <button className="btn"
+                    onClick={()=> setCount(count + 1)}>+1</button>
+                    <button className="btn"
+                    /* aqui temos uma função, onde pegamos o valor atual */
+                    onClick={()=> setCount(currect =>currect + 1000)}>+1000</button>
+                </div>
+            </div>
+
+            <SectionTitle title="Exercício #02"/>
+        </div>
+    )
+}
+
+export default UseState
